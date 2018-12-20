72f6aa9dfa388d5371438263101fe8690501a5b7
yarik <yarik.red@gmail.com>
2018-12-21 00:52:05 +0200

    Updates

---
+++ precache-manifest.17c32a1aef89554db615f59483aa7b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "716bcf97bca7373ce6e7",
+    "url": "/hw1/static/css/main.ce5af72a.chunk.css"
+  },
+  {
+    "revision": "716bcf97bca7373ce6e7",
+    "url": "/hw1/static/js/main.716bcf97.chunk.js"
+  },
+  {
+    "revision": "0b76e2b518e8faff3f83",
+    "url": "/hw1/static/js/1.0b76e2b5.chunk.js"
+  },
+  {
+    "revision": "51768e7d4fc0ab7fe2a9",
+    "url": "/hw1/static/js/runtime~main.51768e7d.js"
+  },
+  {
+    "revision": "85457f1105ad494a73cd3a8225e0b3de",
+    "url": "/hw1/index.html"
+  }
+];
